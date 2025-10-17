d24320af55665ac144ce4b0a6585e72c938ce94a
AnubhavGitHub07 <prince1dwivedi00@gmail.com>
2025-10-17 14:19:25 +0530

    Codeforce Contestprep

---
+++ A_Collatz_Conjecture.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// --- Fast I/O ---
+#define fast_io                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+
+// --- Solve function ---
+void solve()
+{
+
+  
+
+    int k , x;
+
+    cin>>k>>x;
+
+      int inital_value  = pow(2,k) * x;
+
+      cout<<inital_value<<endl;
+}
+
+int main()
+{
+    fast_io;
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
---
+++ B_Fun_Permutation.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// --- Fast I/O ---
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+
+
+// --- Solve function ---
+void solve() {
+    
+
+}
+
+int main() {
+    fast_io;
+
+    int t;
+    cin>>t;
+
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
