cdc818a60ed0742f99d730518262bf1e8d5c6548
AnubhavGitHub07 <prince1dwivedi00@gmail.com>
2025-10-18 00:29:29 +0530

    Q1 solution

--- Codeforces_Round_1059 _Div. 3)/Q1.cpp
+++ Codeforces_Round_1059 _Div. 3)/Q1.cpp
@@ -3,22 +3,53 @@
 using namespace std;
 
 // --- Fast I/O ---
-#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
-
+#define fast_io                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
 
 // --- Solve function ---
-void solve() {
-    
+void solve()
+{
 
+    int sum = 0;
+
+    int n;
+    cin >> n;
+
+    vector<int> nums(n);
+
+    for(int i = 0 ; i <n ; i++){
+        cin>>nums[i];
+    }
+
+    int maximum_average = 0;
+
+    maximum_average = *max_element(nums.begin() , nums.end());
+
+    for(int i = 0 ; i<n ; i++){
+
+        sum+=nums[i];
+    }
+    
+    if(sum/n > maximum_average ){
+        maximum_average = sum/n;
+        cout<<maximum_average<<endl;
+    }
+
+    else {
+        cout<<maximum_average<<endl;;
+    }
 }
 
-int main() {
+int main()
+{
     fast_io;
 
     int t;
-    cin>>t;
+    cin >> t;
 
-    while (t--) {
+    while (t--)
+    {
         solve();
     }
     return 0;
